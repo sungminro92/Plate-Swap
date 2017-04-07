a24a2505e2d0dcfe5ec6c9c4b09c603f5632a082
hkarambizi <hkarambizi@gmail.com>
2017-04-07 18:03:42 -0400

    WIP WIP WIP testing mongoose connection with seeds

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 app.use(express.static(__dirname + '/public'));
 
@@ -7,3 +9,7 @@ app.listen(process.env.PORT || 3000, function() {
   console.log('App listening successfully bruh');
 });
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+mongoose.connect('mongodb://localhost:27017/barrels')
---
+++ server/db/schema.js
@@ -0,0 +1,47 @@
+var mongoose = require('mongoose'); //requiring mongoose.
+
+var Schema = mongoose.Schema;
+
+// commentSchema
+var CommentSchema = new Schema({
+	text: String,
+	created_at: Date
+});
+
+var ItemSchema = new Schema({
+	name: String,
+	description: String,
+	price: Number,
+	Image: String,
+	city: String,
+	comments: [CommentSchema],
+	state: String,
+	created_at: Date,
+	updated_at: Date,
+});
+
+ItemSchema.pre('save', function(next) {
+	now = new Date();
+	this.updated_at = now;
+
+	if (!this.created_at) {this.created_at = now}
+	next();
+});
+
+var UserSchema = new Schema({
+	firstName: String,
+	lastName: String,
+	email: String,
+	password_digest: String,
+	items: [ItemSchema],
+})
+
+var UserModel = mongoose.model('User', UserSchema);
+var ItemModel = mongoose.model('Item', ItemSchema);
+var CommentModel = mongoose.model('Comment', CommentSchema);
+
+module.exports = {
+	User: UserModel,
+	Item: ItemModel,
+	Comment: CommentModel
+}
