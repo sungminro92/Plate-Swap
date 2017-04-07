f02fd273fa5e43332dc78e60f54bacafb73f85ef
tjdals504 <sungminro92@gmail.com>
2017-04-07 12:35:37 -0400

    Added angular & Created folders and files

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+angular = require('angular');
 
 app.use(express.static(__dirname + '/public'));
 
 app.listen(process.env.PORT || 3000, function() {
   console.log('App listening successfully bruh');
 });
+
