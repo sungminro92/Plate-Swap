e4685e8667851a737f304b5fbba071e0287835a9
deviea8 <deviea8@gmail.com>
2017-04-08 12:14:22 -0400

    UI router setup and db query for show all

--- client/app.js
+++ client/app.js
@@ -1,4 +1,15 @@
 const angular = require('angular');
+require('angular-ui-router');
 
 angular
-	.module('projectThree', []);
+	.module('projectThree', ['ui.router'])
+  .config(routerSetup)
+
+
+routerSetup.$inject = ['$stateProvider', '$urlRouterProvider'];
+function routerSetup($stateProvider, $urlRouterProvider) {
+  $stateProvider
+  .state('/', {
+    template: '<h2>Hello from UI Router</h2>'
+  });
+};
